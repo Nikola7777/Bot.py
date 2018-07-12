e1ac8c1445c4633a73c913d5b0323bb07d2b4e34
Nikola7777 <40716840+Nikola7777@users.noreply.github.com>
2018-07-12 14:36:15 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+import time
+
+bot = commands.Bot(command_prefix='#')
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(game=discord.Game(name='Made By Niko#7360'))
+    print ("Ready when you are xd")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
+    
+bot.run(str(os.environ.get('TOKEN')))
